05eeccfe1f50cf036e43c55dc83b90971bd0ec59
Josue Gallegos <gallegosjosue6@gmail.com>
2023-09-20 18:23:46 -0500

    complete sending data fetch

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      },
+      body: JSON.stringify({
+        name, email
+      })
+    }).then(resp => resp.json()).then(function(json) {
+        document.body.innerHTML = json.id;
+      }).catch(error => {
+        document.body.innerHTML = error.message;
+      });
+    }
